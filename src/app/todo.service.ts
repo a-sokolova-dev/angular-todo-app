85e89d0e288a41e827970b27f962a87a8f8e31d1
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-09-08 21:12:01 +0300

    refactor: change todo id type to string

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -4,11 +4,31 @@ import { Todo } from './todo';
 @Injectable({ providedIn: 'root' })
 export class TodoService {
   todos = [
-    { id: 1, todo: 'Do something nice', completed: true },
-    { id: 2, todo: 'Memorize a poem', completed: true },
-    { id: 3, todo: 'Watch a classic movie', completed: true },
-    { id: 4, todo: 'Watch a documentary', completed: false },
-    { id: 5, todo: 'Invest in cryptocurrency', completed: false },
+    {
+      id: '36b8f84d-df4e-4d49-b662-bcde71a8764f',
+      todo: 'Do something nice',
+      completed: true,
+    },
+    {
+      id: 'f745e8fe-f4f3-4a40-a5cd-67bd802f8960',
+      todo: 'Memorize a poem',
+      completed: true,
+    },
+    {
+      id: 'd8bc8761-c82b-47d3-96dc-58b910aab861',
+      todo: 'Watch a classic movie',
+      completed: true,
+    },
+    {
+      id: '174f1898-3ea4-4334-8d33-5c7c27650c7d',
+      todo: 'Watch a documentary',
+      completed: false,
+    },
+    {
+      id: 'ce1450e7-cba4-4b1e-970c-76737053db45',
+      todo: 'Invest in cryptocurrency',
+      completed: false,
+    },
   ];
 
   getAllTodos(): Todo[] {
